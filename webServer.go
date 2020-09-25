2352c7130b25f786490f11576f624ea93dd286f9
Patrick <patr6996@edu.easj.dk>
2020-09-25 10:47:37 +0200

    Finally got connection to localhost: 8081
    Solved with stackOverFlow, the reason for this, was due to wrong folder structure.

--- webServer.go
+++ webServer.go
@@ -8,13 +8,10 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
-	log.Fatal(http.ListenAndServe(":8082", nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 /*
 Dette er en webserver der åbner html/css filer,
  fra en mappe der hedder static
-
-Virker ikke pt, kæmper med at fixe port.
-Den giver siden, men 404 fejl server not found
 */
